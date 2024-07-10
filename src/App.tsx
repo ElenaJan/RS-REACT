d3494f5db93dad114ee2f83b30881058240f452d
Elena Janecek <janechek.elena@gmail.com>
2024-07-10 20:48:47 +0300

    feat: add custom hook for ls

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,19 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React from 'react'
 import Search from './components/Search'
 import Results from './components/Results'
 import ErrorBoundary from './components/ErrorBoundary'
-import Loader from './components/Loader'
+import NotFound from './components/NotFound'
+import './App.css'
 import usePersistentSearch from './hooks/usePersistentSearch'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
-interface Result {
-    name: string
-    description: string
-}
-
 const App: React.FC = () => {
-    const [results, setResults] = useState<Result[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<Error | null>(null)
     const [searchTerm, setSearchTerm] = usePersistentSearch('searchTerm')
-    const [testError, setTestError] = useState<boolean>(false)
-
-    const fetchResults = useCallback((searchTerm: string) => {
-        setLoading(true)
-        const url = searchTerm
-            ? `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
-            : `https://pokeapi.co/api/v2/pokemon?limit=20`
-
-        fetch(url)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(
-                        'Something went wrong! Network response was not ok!',
-                    )
-                }
-                return response.json()
-            })
-            .then((data) => {
-                if (searchTerm) {
-                    setResults([
-                        {
-                            name: data.name,
-                            description: `Height: ${data.height}, Weight: ${data.weight}`,
-                        },
-                    ])
-                } else {
-                    const results = data.results.map(
-                        (item: { name: string; url: string }) => ({
-                            name: item.name,
-                            description: item.url,
-                        }),
-                    )
-                    setResults(results)
-                }
-                setLoading(false)
-            })
-            .catch((error) => {
-                setError(error)
-                setLoading(false)
-            })
-    }, [])
-
-    useEffect(() => {
-        fetchResults(searchTerm)
-    }, [searchTerm, fetchResults])
 
     const handleSearch = (searchTerm: string) => {
+        console.log(searchTerm)
         setSearchTerm(searchTerm)
-    }
-
-    const throwError = () => {
-        setTestError(true)
-        throw new Error('This is a test error!')
     }
 
     return (
@@ -82,26 +27,15 @@ const App: React.FC = () => {
             >
                 <div className="search-wrapper">
                     <Search onSearch={handleSearch} />
-                    <button className="error-btn" onClick={throwError}>
-                        Throw Error
-                    </button>
                 </div>
-                <div
-                    style={{
-                        flex: '1 1 80%',
-                        overflowY: 'auto',
-                        padding: '10px',
-                    }}
-                >
-                    {loading ? (
-                        <Loader />
-                    ) : error ? (
-                        <p>Error loading results: {error.message}</p>
-                    ) : testError ? (
-                        <p>Test error</p>
-                    ) : (
-                        <Results results={results} />
-                    )}
+                <div style={{ flex: '1 1 80%', padding: '10px' }}>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Results search={searchTerm} />}
+                        />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
                 </div>
             </div>
         </ErrorBoundary>
--- src/hooks/usePersistentSearch.ts
+++ src/hooks/usePersistentSearch.ts
@@ -1,19 +1,19 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useCallback } from 'react'
 
 const usePersistentSearch = (key: string, defaultValue: string = '') => {
     const [searchTerm, setSearchTerm] = useState(() => {
         return localStorage.getItem(key) || defaultValue
     })
 
-    useEffect(() => {
-        return () => {
-            localStorage.setItem(key, searchTerm)
-        }
-    }, [key, searchTerm])
+    localStorage.setItem('searchTerm', searchTerm)
 
-    const updateSearchTerm = useCallback((newTerm: string) => {
-        setSearchTerm(newTerm)
-    }, [])
+    const updateSearchTerm = useCallback(
+        (newTerm: string) => {
+            localStorage.setItem('searchTerm', searchTerm)
+            setSearchTerm(newTerm)
+        },
+        [searchTerm],
+    )
 
     return [searchTerm, updateSearchTerm] as const
 }
